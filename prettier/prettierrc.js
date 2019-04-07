4c212cc39576e7679530d946e01adab638f4d75d
lxynox <lxynox@gmail.com>
2019-04-06 18:45:42 -0700

    Added prettier, editorconfig dotfiles

---
+++ prettier/prettierrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // Fit code within this line limit
+    printWidth: 80,
+
+    // Number of spaces it should use per tab
+    tabWidth: 2,
+
+    // If true, will use single instead of double quotes
+    singleQuote: true,
+
+    // Controls the printing of trailing commas wherever possible
+    trailingComma: 'es5',
+
+    // Controls the printing of spaces inside object literals
+    bracketSpacing: false,
+
+    // If true, puts the `>` of a multi-line jsx element at the end of the last line instead of being alone on the next line
+    jsxBracketSameLine: false,
+
+    // Whether to add a semicolon at the end of every line
+    semi: false,
+
+    // Indent lines with tabs
+    useTabs: true,
+};
